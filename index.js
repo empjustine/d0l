8181a3eaf94feff6b80be81cd5df153050e930cc
Cássio Oseki <emp.justine@gmail.com>
2017-12-31 02:34:28 +0000

    0.0.1 build

---
+++ index.js
@@ -0,0 +1,54 @@
+"use strict";
+/**
+ * D0L System / Deterministic context-free L-system
+ *
+ * Rules for L-systems:
+ *   - V, "alphabet";
+ *     - Set<Symbol>;
+ *     - the set Alphabet MUST be composed of Symbols;
+ *   - ω ∈ V⁺, "axiom";
+ *     - Array<Symbol>;
+ *     - axiom MUST be composed of a non-empty sequence of Symbols;
+ *   - P ⊂ V* × V*, "productions";
+ *     - Set<Array<Symbol>,Set<Array<Symbol>>>;
+ *     - a finite Set Production MUST is a set of rules, from sequences of
+ *       Symbols to a Set of distinct sequences of Symbols.
+ *   - When computing the result of a iteration, all productions are
+ *     applied simultaneously.
+ *
+ * Rule for Deterministic context-free L-systems:
+ *   - P ⊂ V × V*
+ *     - Map<Symbol,Array<Symbol>>;
+ *     - productions must be a function;
+ *     - The domain of productions MUST be a Set composed of individual
+ *       Symbols;
+ *     - The codomain of productions MUST be a sequence of Symbols.
+ *
+ */
+class DeterministicContextFreeStringRewritingSystem {
+    /**
+     *
+     * Setup to enable iterative computation of the L-system
+     * grammar.
+     * @param productions Symbol rewritting function used by
+     * this system
+     */
+    constructor(productions) {
+        this.productions = productions;
+    }
+    /**
+     *
+     * Given a `previousGeneration``, compute the next iteration of a
+     * sequence of Symbols synchronously.
+     * @param {Array<D0LSymbol>} previousGeneration sequence of Symbols, where all
+     * productions will be applied.
+     * @returns {Array<D0LSymbol>} a derived sequence of Symbols, representing the current
+     * generation.
+     */
+    derive(previousGeneration) {
+        const productions = this.productions;
+        return [...(previousGeneration)].map(v => productions.get(v)).join('');
+    }
+}
+module.exports = DeterministicContextFreeStringRewritingSystem;
+//# sourceMappingURL=index.js.map
